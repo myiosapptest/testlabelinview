72a8ff2eb51f23f8c251adf793cca5904faa2a56
rithlkh2 <rithronlkh@gmail.com>
2019-02-14 15:14:25 +0700

    add sample design view in of calendar

--- testlabelinview/ViewController.swift
+++ testlabelinview/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var viewSubView: UIView!
+    var viewTestView : UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 500))
+        
+        let centerView = UIView()
+        centerView.translatesAutoresizingMaskIntoConstraints = false
+        centerView.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        
+        view.addSubview(centerView)
+        
+        centerView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        centerView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        centerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        centerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Testing"
+        label.textColor = UIColor.black
+        label.backgroundColor = UIColor.yellow
+        
+        centerView.addSubview(label)
+        
+        label.leftAnchor.constraint(equalTo: centerView.leftAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: centerView.topAnchor).isActive = true
+        
+        self.view.layoutIfNeeded()
+        self.view.addSubview(view)
     }
-
-
 }
 
